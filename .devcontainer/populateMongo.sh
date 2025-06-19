34d951d908b1ff9650ea1a30dc85ec610c6b9cbf
Magdalena Taurian <116666741+mtaurian@users.noreply.github.com>
2025-06-19 22:48:41 +0000

    MongoDB population when Codespace starts

--- .devcontainer/populateMongo.sh
+++ .devcontainer/populateMongo.sh
@@ -4,11 +4,20 @@ echo "Populating ..."
 
 sed 's/;/,/g' /workspaces/BD2-TPO/datasets/producto.csv > /workspaces/BD2-TPO/datasets/producto_coma.csv
 sed 's/;/,/g' /workspaces/BD2-TPO/datasets/proveedor.csv > /workspaces/BD2-TPO/datasets/proveedor_coma.csv
+# sed 's/;/,/g' /workspaces/BD2-TPO/datasets/telefono.csv > /workspaces/BD2-TPO/datasets/telefono_coma.csv
+# sed 's/;/,/g' /workspaces/BD2-TPO/datasets/op.csv > /workspaces/BD2-TPO/datasets/op_coma.csv
+# sed 's/;/,/g' /workspaces/BD2-TPO/datasets/detalle_op.csv > /workspaces/BD2-TPO/datasets/detalle_op_coma.csv
 
 mongoimport --host mongo --port 27017 --db tpo_g7 --collection productos --type csv --headerline --file /workspaces/BD2-TPO/datasets/producto_coma.csv
 mongoimport --host mongo --port 27017 --db tpo_g7 --collection proveedores --type csv --headerline --file /workspaces/BD2-TPO/datasets/proveedor_coma.csv
+# mongoimport --host mongo --port 27017  --db tpo_g7 --collection telefonos --type csv --headerline --file /workspaces/BD2-TPO/datasets/telefono_coma.csv
+# mongoimport --host mongo --port 27017  --db tpo_g7 --collection ops --type csv --headerline --file /workspaces/BD2-TPO/datasets/op_coma.csv
+# mongoimport --host mongo --port 27017  --db tpo_g7 --collection detalle_ops --type csv --headerline --file /workspaces/BD2-TPO/datasets/detalle_op_coma.csv
 
 rm /workspaces/BD2-TPO/datasets/producto_coma.csv
 rm /workspaces/BD2-TPO/datasets/proveedor_coma.csv
+# rm /workspaces/BD2-TPO/datasets/telefono_coma.csv
+# rm /workspaces/BD2-TPO/datasets/op_coma.csv
+# rm /workspaces/BD2-TPO/datasets/detalle_op_coma.csv
 
 echo "Done!"
