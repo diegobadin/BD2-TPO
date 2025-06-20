f0838811d38adf2b4a54cfa9ca0057a004f2387f
Magdalena Taurian <116666741+mtaurian@users.noreply.github.com>
2025-06-20 14:10:58 +0000

    Added description to query 1

--- server/src/queries/query1.js
+++ server/src/queries/query1.js
@@ -1,3 +1,6 @@
+/**
+ * Obtener los datos de los proveedores activos y habilitados junto con sus teléfonos.
+ */
 const { getDB } = require('../lib/mongo');
 const { subscribe } = require('../utils/cacheDependency');
 
@@ -15,7 +18,15 @@ module.exports = {
 
         const results = db.collection(proveedores).find(
             { activo: 1, habilitado: 1 },
-            { _id: 0, id_proveedor: 1, CUIT_proveedor: 1, razon_social: 1, tipo_sociedad: 1, direccion: 1, telefonos: 1}
+            { $project: { 
+                _id: 0, 
+                id_proveedor: 1, 
+                CUIT_proveedor: 1, 
+                razon_social: 1, 
+                tipo_sociedad: 1, 
+                direccion: 1, 
+                telefonos: 1 } 
+            }
         ).toArray();
 
         subscribe(proveedores, key_prefix);
