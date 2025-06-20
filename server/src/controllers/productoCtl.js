2e7e7346de9af72d606ab498da12fc016d6550f0
Diego-Rabinovich <120027298+Diego-Rabinovich@users.noreply.github.com>
2025-06-20 22:45:43 +0000

    implemented query 14 (create, and modify existing products) plus: also can delete

--- server/src/controllers/productoCtl.js
+++ server/src/controllers/productoCtl.js
@@ -1,12 +1,5 @@
 const srv = require('../services/productoSrv');
 
-async function getAll(req, res, next) {
-  try {
-    const lista = await srv.getAll();
-    res.json(lista);
-  } catch (err) { next(err); }
-}
-
 async function create(req, res, next) {
   try {
     const nuevo = await srv.create(req.body);
@@ -16,11 +9,13 @@ async function create(req, res, next) {
 
 async function update(req, res, next) {
   try {
-    const upd = await srv.update(req.params.id, req.body);
+    const id = Number(req.params.id); 
+    const upd = await srv.update(id, req.body);
     res.json(upd);
-  } catch (err) { next(err); }
+  } catch (err) {
+    next(err);
+  }
 }
-
 async function remove(req, res, next) {
   try {
     await srv.remove(req.params.id);
@@ -28,4 +23,4 @@ async function remove(req, res, next) {
   } catch (err) { next(err); }
 }
 
-module.exports = { getAll, create, update, remove };
+module.exports = {create, update, remove };
--- server/src/routes/productos.js
+++ server/src/routes/productos.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const ctl = require('../controllers/productoCtl');
 
-router.get('/', ctl.getAll);
 router.post('/', ctl.create);
 router.put('/:id', ctl.update);
 router.delete('/:id', ctl.remove);
