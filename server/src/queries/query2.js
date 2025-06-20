b14684245adfc8146489a7948e7978fd70a23500
Diego-Rabinovich <120027298+Diego-Rabinovich@users.noreply.github.com>
2025-06-20 07:03:17 +0000

    cooments and format

--- server/src/queries/query2.js
+++ server/src/queries/query2.js
@@ -1,24 +1,26 @@
+/**
+ * Obtener el/los teléfono/s y el código del/los proveedor/es que contengan la cadena word en su razón social.
+ */
 const { getDB } = require('../lib/mongo');
 const { subscribe } = require('../utils/cacheDependency');
 const proveedores = "proveedores";
 const key_prefix = "query2";
 
 module.exports = {
-  getKey({ query }) {
+    getKey({ query }) {
     return key_prefix+ ':' + query.word;
-  },
-  resources: [proveedores],
-  async execute({ query }) {
+    },
+    resources: [proveedores],
+    async execute({ query }) {
         const db = getDB();
-        
         if (!query.word) throw new Error("Falta el parámetro 'word'");
+    
+        const results = await db.collection(proveedores).find(
+            {razon_social: { $regex: query.word, $options: 'i' }},
+            { projection: { _id: 0, id_proveedor: 1, razon_social: 1, telefonos: 1 } }
+            ).toArray();
 
-       const results = await db.collection(proveedores).find(
-        {razon_social: { $regex: query.word, $options: 'i' }},
-        { projection: { _id: 0, id_proveedor: 1, razon_social: 1, telefonos: 1 } }
-        ).toArray();
-        console.log(results);
-        subscribe(proveedores, key_prefix+":"+query.word);
+        subscribe(proveedores, key_prefix + ":" + query.word);
         return results;
     }
 };
