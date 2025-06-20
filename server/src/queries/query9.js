9401ea7d362a76fe7e49a7064c5531c502b4741d
diegobadin <89800122+diegobadin@users.noreply.github.com>
2025-06-20 18:36:33 +0000

    Added query 9 and 12

--- server/src/queries/query9.js
+++ server/src/queries/query9.js
@@ -1,61 +1,73 @@
-const { getDB } = require('../lib/mongo');
-const { subscribe } = require('../utils/cacheDependency');
+const { getDB } = require("../lib/mongo");
+const { subscribe } = require("../utils/cacheDependency");
 const operaciones = "ops";
 const detalle_operaciones = "detalle_ops";
 const productos = "productos";
 const key_prefix = "query9";
 
 module.exports = {
-    getKey({ query }) {
-        return key_prefix+ ':' + query.marca;
-    },
-    resources: [proveedores],
-    async execute({ query }) {
-        const db = getDB();
+  getKey({ query }) {
+    return key_prefix + ":" + query.marca;
+  },
+  resources: [productos],
+  async execute({ query }) {
+    const db = getDB();
 
-        if (!query.marca) throw new Error("Falta el parámetro 'marca'");
+    if (!query.marca) throw new Error("Falta el parámetro 'marca'");
 
-        const results = await db.collection(operaciones).agregate([[
-            {
-                $lookup: {
-                    from: detalle_operaciones,
-                    localField: 'id_orden',
-                    foreignField: 'id_orden',
-                    as: 'detalles'
-                }
-            },
-            { $unwind: "$detalles" },
+    console.log(query.marca);
 
-            {
-                $lookup: {
-                    from: productos,
-                    localField: 'detalles.id_producto',
-                    foreignField: 'id_producto',
-                    as: 'producto_info'
-                }
-            },
-            { $unwind: "$producto_info" },
+    const results = await db
+      .collection(productos)
+      .aggregate([
+        {
+          $match: { marca: query.marca },
+        },
+        {
+          $lookup: {
+            from: detalle_operaciones,
+            localField: "id_producto",
+            foreignField: "id_producto",
+            as: "detalles",
+          },
+        },
+        { $unwind: "$detalles" },
 
-            {
-                $match: {
-                    "producto_info.marca": query.marca
-                }
-            },
+        {
+          $lookup: {
+            from: operaciones,
+            localField: "detalles.id_pedido",
+            foreignField: "id_pedido",
+            as: "orden",
+          },
+        },
+        { $unwind: "$orden" },
 
-            {
-                $group: {
-                    _id: "$id_pedido",
-                    id_proveedor: "$id_proveedor",
-                    fecha: "$fecha" ,
-                    total_sin_iva:  "$total_sin_iva" ,
-                    iva: "$iva"
-                }
-            },
-        ]]).toArray();
-        console.log(results);
-        subscribe(operaciones, key_prefix+":"+query.marca);
-        subscribe(detalle_operaciones, key_prefix+":"+query.marca);
-        subscribe(productos, key_prefix+":"+query.marca);
-        return results;
-    }
+        {
+          $group: {
+            _id: "$orden.id_pedido",
+            id_proveedor: { $first: "$orden.id_proveedor" },
+            fecha: { $first: "$orden.fecha" },
+            total_sin_iva: { $first: "$orden.total_sin_iva" },
+            iva: { $first: "$orden.iva" },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            id_pedido: "$_id",
+            id_proveedor: 1,
+            fecha: 1,
+            total_sin_iva: 1,
+            iva: 1,
+          },
+        },
+      ])
+      .toArray();
+
+    subscribe(operaciones, key_prefix + ":" + query.marca);
+    subscribe(detalle_operaciones, key_prefix + ":" + query.marca);
+    subscribe(productos, key_prefix + ":" + query.marca);
+    return results;
+  },
 };
